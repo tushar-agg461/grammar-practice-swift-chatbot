0b2c8d81dcc404268bc734f06974444a18bc7516
Priyanka-sharma181 <priyanka.sharma@madgicaltechdom.com>
2023-11-09 15:07:50 +0530

    merge code

--- src/chat/chatbot.service.ts
+++ src/chat/chatbot.service.ts
@@ -19,14 +19,14 @@ export class ChatbotService {
     this.userService = userService;
   }
 
-  public async processMessage(body:any): Promise<any> {
+  public async processMessage(body: any): Promise<any> {
     const { from, text } = body;
     let botID = process.env.BOT_ID;
     const userData = await this.userService.findUserByMobileNumber(from);
     const { intent, entities } = this.intentClassifier.getIntent(text.body);
     if (userData.language === 'english' || userData.language === 'hindi') {
       await this.userService.saveUser(userData);
-    }      
+    }
     if (intent === 'greeting') {
       this.message.sendWelcomeMessage(from, userData.language);
     } else if (intent === 'select_language') {
@@ -39,5 +39,4 @@ export class ChatbotService {
     return 'ok';
   }
 }
-
 export default ChatbotService;
